d3c610c166e03079a0b6bc7059b34cf2c219837f
Munazah-Mehdi <munazahmehdi@gmail.com>
2026-02-12 13:35:18 +0500

    initial setup

---
+++ js/script.js
@@ -0,0 +1,12 @@
+particlesJS("particles-js", {
+    "particles": {
+      "number": {"value": 50},
+      "color": {"value": "#39ff14"},
+      "shape": {"type": "circle"},
+      "opacity": {"value": 0.3},
+      "size": {"value": 3},
+      "line_linked": {"enable": true, "distance": 150, "color": "#39ff14", "opacity": 0.2, "width": 1},
+      "move": {"enable": true, "speed": 2}
+    }
+  });
+  
